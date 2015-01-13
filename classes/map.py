c4049a1a87a452e1de8f8111d75bf9d0b9469847
poolikud <kaljurav@uni.coventry.ac.uk>
2015-01-13 17:47:13 +0000

    Create map.py

---
+++ classes/map.py
@@ -0,0 +1,19 @@
+import pygame
+from pygame import *
+
+class Map:
+
+    def __init__(self):
+        #set up map
+    def images():
+        treasure_chest = "chest.png"
+        pirate = "robot.png"
+    def drawMap():
+        pygame.image.load(images.treasure_chest)
+    def drawObstacle():
+
+    def drawRobot():
+
+    def drawTreasure():
+
+    def drawButton():
