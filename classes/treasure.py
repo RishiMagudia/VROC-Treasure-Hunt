b7014b5ec7b5acf293b6de19f5db04b1bf970ec3
Ak1001 <Ak1001@users.noreply.github.com>
2015-01-14 19:24:35 +0000

    Update treasure.py

--- classes/treasure.py
+++ classes/treasure.py
@@ -1,1 +1,13 @@
-Treasure
+class treasure():
+    def __innit__(self,ID,found= True):
+        self.ID=ID
+        self.found= found
+
+
+    def isFound(self):                                           #see if the treasure has been found
+        return self.found
+        
+
+
+    def ShowMessage(self):                                      #displaying message if robot finds the tresuare 
+        print "Treasure",self.ID, "Found" 
