7024ea026a880a67314af8d4c8dead5f11c1d6cf
poolikud <kaljurav@uni.coventry.ac.uk>
2015-01-16 18:37:55 +0000

    Update map.py
    Created a bubble sorter for prioritizing the order in which images are printed on the screen. (doesent quite work yet, but easily fixable)

--- classes/map.py
+++ classes/map.py
@@ -9,8 +9,8 @@ class Map():
         # img_name 1 is landmark.png
         # img_name 2 is robot.png
         # img_name 3 is traffic.png
-        self.img_name = ["sea.png", "landmark.png", "robot.png", "traffic.png"]
-        self.images()
+        self.img_name = [1, "sea.png", 5, "landmark.png", 3, "robot.png", 2, "traffic.png"]
+        self.priority(self.img_name)
     def image(self, img_num):
         # taking the requested img_name and looping though all the names
         # until correct name is found and load the image with that name from
@@ -21,12 +21,17 @@ class Map():
                 pygame.image.load(images.self.variables[i])
     def drawMap():
         pygame.image.load(images.treasure_chest)
-    def drawObstacle():
-        print "test"
-    def drawRobot():
-        print "test"
-    def drawTreasure():
-        print "test"
-    def drawButton():
-        print "test"
+    def priority(self, img_name):
+        self.img_name = img_name
+        for i in range(len(img_name)/2-1,0,-1):
+            for m in range(0,i,2):
+                if img_name[m] > img_name[m+2]:
+                    temp_name = img_name[m]
+                    temp_value = img_name[m+1]
+                    img_name[m] = img_name[m+2]
+                    img_name[m+1] = img_name[m+3]
+                    img_name[m+2] = temp_value
+                    img_name[m+3] = temp_name
+        print img_name
+
 Map()
