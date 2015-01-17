ceae46359623fc3b42b4d97d5c7e4831d6c94cae
Ak1001 <Ak1001@users.noreply.github.com>
2015-01-17 16:37:56 +0000

    Update treasure.py

--- classes/treasure.py
+++ classes/treasure.py
@@ -1,16 +1,22 @@
 class treasure():
-    def __init__(self,ID,found= True ):
+    def __init__(self,ID,found= True ):            #Properties of my class
         self.ID=ID
         self.found= found
 
 
-    def isFound(self):                                                  #This is used to see if the treausre is found
-        print "no"
+    def isFound(self):                            #This returns values to the landmark class             
+        if self.found== True:                     #to show the tresaure has been found and searched
+            return treasure_found_serchead        
+        else:
+            return not_searched
+            
         
 
-    
-    def Showing_message(self):                                          #This shows a message saying the Treasure is found
-        print "Treasure", ID, "Found"
+
+    def Showing_message(self):                   #This shows a message that the treasure as been found
+        if self.found== True:                    #when robot is present
+            print "Treasure", self.ID, "Found"
+        
 
         
 
@@ -18,5 +24,5 @@ class treasure():
      
 test = treasure(12,True)
 print test.found
+found=True
 test.Showing_message()
-test.isFound()
