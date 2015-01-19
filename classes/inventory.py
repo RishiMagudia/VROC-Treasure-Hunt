cca6c36a8b77516126987bcb4683e43523ef7895
poolikud <kaljurav@uni.coventry.ac.uk>
2015-01-19 17:38:12 +0000

    intentory.py
    skeleton for inventory class

---
+++ classes/inventory.py
@@ -0,0 +1,13 @@
+class Inventory:
+
+    def __init__(self):
+        print "test"
+
+    def getScore(self):
+        print "test"
+        
+    def getTrsr(self):
+        print "test"
+
+    def dispTrsr(self):
+        print "test"
