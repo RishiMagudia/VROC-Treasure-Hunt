6ccab5f14b64cfc1cb1c890081fdd823e579bd90
poolikud <kaljurav@uni.coventry.ac.uk>
2015-01-19 16:57:43 +0000

    Update map.py
    made a usable function for prioritizing images with coordinates and made a function to print the images in the Base class

--- classes/map.py
+++ classes/map.py
@@ -1,38 +1,44 @@
 import pygame
 from pygame import *
-
-class Map():
-
+class Base:
     def __init__(self):
-        # making the list for the names of the images
-        # img_name 0 is sea.png
-        # img_name 1 is landmark.png
-        # img_name 2 is robot.png
-        # img_name 3 is traffic.png
-        self.img_name = [1, "sea.png", 5, "landmark.png", 3, "robot.png", 2, "traffic.png"]
-        self.priority(self.img_name)
-    def image(self, img_num):
-        # taking the requested img_name and looping though all the names
-        # until correct name is found and load the image with that name from
-        # images folder
-        self.img_num = img_num
-        for i in range(len(self.variables)):
-            if i == self.img_num:
-                pygame.image.load(images.self.variables[i])
-    def drawMap():
-        pygame.image.load(images.treasure_chest)
-    def priority(self, img_name):
+        pirate = ("pirate", (100,200), 4)
+        background = ("sea", (0,0), 1)
+        treasure = ("treasure", (200,250), 3)
+        island = ("island", (150,250), 2)
+        self.img_list = [pirate, background, treasure, island]
+class Map:
+    
+    def __init__(self):
+        # starting off the class
+        self.base = Base()
+        self.priority()
+    def drawMap(self):
+        #drawing the map
+        print self.base.img_list
+        running = True
+        while running == True:
+            for i in self.base.img_list:
+                name, coordiantes, priority = i
+                for i in range(len(self.base.img_list),0,1):
+                    screen.blit(name,(coordinates))
+    def priority(self):
         # using bubble sorting to priotitize image printing order
-        self.img_name = img_name
-        for i in range(len(img_name)/2,0,-1):
-            for m in range(0,i+2,2):
-                if img_name[m] > img_name[m+2]:
-                    temp_name = img_name[m+1]
-                    temp_value = img_name[m]
-                    img_name[m] = img_name[m+2]
-                    img_name[m+1] = img_name[m+3]
-                    img_name[m+2] = temp_value
-                    img_name[m+3] = temp_name
-        print img_name
+        pList = []
+        # unpackage all packages and store priority in a list
+        for n in self.base.img_list:
+            pList.append(n[2])
+        print self.base.img_list
+        # sort out the packages by priority
+        for i in range(len(pList)-1,0,-1):
+            for m in range(0,i,1):
+                if pList[m] > pList[m+1]:
+                    holder = self.base.img_list[m+1]
+                    self.base.img_list[m+1] = self.base.img_list[m]
+                    self.base.img_list[m] = holder
+                    pholder = pList[m+1]
+                    pList[m+1] = pList[m]
+                    pList[m] = pholder
+        self.drawMap()
 
 Map()
