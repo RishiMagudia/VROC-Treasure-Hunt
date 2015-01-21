26007d3c113326e6f6170169996bbe65b064e61e
poolikud <kaljurav@uni.coventry.ac.uk>
2015-01-21 12:52:30 +0000

    Update map.py
    fixed some minor errors

--- classes/map.py
+++ classes/map.py
@@ -12,50 +12,46 @@ class Map:
     
     def __init__(self):
         # starting off the class
-        self.base = Base()
         self.priority2()
-    def drawMap(self):
+        self.sList = []
+        self.pList = []
+    def drawMap(self,sList):
         #drawing the map
         running = True
         while running == True:
-            for i in self.base.sList:
+            for i in self.sList:
                 name, coordiantes, priority = i
-                for i in range(len(self.base.img_list),0,1):
+                for i in range(len(self.sList),0,1):
                     screen.blit(name,(coordinates))
-    def priority(self):
-        # using bubble sorting to priotitize image printing order
-        pList = []
-        # unpackage all packages and store priority in a list
-        for n in self.base.img_list:
-            pList.append(n[2])
-        # sort out the packages by priority using bubble sort
-        for i in range(len(pList)-1,0,-1):
-            for m in range(0,i,1):
-                if pList[m] > pList[m+1]:
-                    holder = self.base.img_list[m+1]
-                    self.base.img_list[m+1] = self.base.img_list[m]
-                    self.base.img_list[m] = holder
-                    pholder = pList[m+1]
-                    pList[m+1] = pList[m]
-                    pList[m] = pholder
-        #self.drawMap()
-    def priority2(self):
+##    def priority(self,img_list):
+##        # using bubble sorting to priotitize image printing order
+##        pList = []
+##        # unpackage all packages and store priority in a list
+##        for n in self.sList:
+##            pList.append(n[2])
+##        # sort out the packages by priority using bubble sort
+##        for i in range(len(pList)-1,0,-1):
+##            for m in range(0,i,1):
+##                if pList[m] > pList[m+1]:
+##                    holder = self.base.img_list[m+1]
+##                    self.base.img_list[m+1] = self.base.img_list[m]
+##                    self.base.img_list[m] = holder
+##                    pholder = pList[m+1]
+##                    pList[m+1] = pList[m]
+##                    pList[m] = pholder
+    def prioritize(self,img_list):
         # using a different method for sorting
-        pList = []
         # unpackage all packages and store priority in a list
-        for n in self.base.img_list:
+        for n in img_list:
             pList.append(n[2])
         # sort the created list
         pList = sorted(pList)
-        # create a new list to store the package into but in a sorted manner
-        sList = []
         # compare the priority to the unsorted list and if prioritys mach
         # add the package into the new sorted list
         for i in range(len(pList)):
-            for m in self.base.img_list:
-                if m[2] == pList[i] and m not in sList:
-                    sList.append(m)
-        print sList
+            for m in img_list:
+                if m[2] == pList[i] and m not in self.sList:
+                    self.sList.append(m)
         #self.drawMap()
 
 
