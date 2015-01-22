d88a17b6061fe427d065aff88670bc906b2be46c
RishiMagudia <rishi.magudia@gmail.com>
2015-01-22 12:33:29 +0000

    Update to robot.py
    Basic variables set
    setters and getters made
    likely to experience heavy modification

--- classes/robot.py
+++ classes/robot.py
@@ -1,1 +1,25 @@
-print 'robot.py'
+from base import Base
+
+class robot(Base):
+
+    def __init__(self,endPos):
+        Base.__init__(self,name=None,size=None,pos=None,stat=None,col=None,img=None)
+        self.velocity = 10
+        self.hasReachedDestination = False
+        self.endPosition = endPos
+
+    def getVelocity(self):
+        return self.velocity
+    def setVelocity(self,velocity):
+        self.velocity = velocity
+
+    def getHasReachedDestination(self):
+        return self.hasReachedDestination
+    def setHasReachedDestination(self,destinationReached):
+        self.hasReachedDestination = destinationReached
+
+    def getEndPosition(self):
+        return self.endPosition
+    def setEndPosition(self,endPos):
+        self.endPosition = endPos
+
