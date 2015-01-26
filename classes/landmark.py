632610b3210fcfc4a836b25c360b4a20a82268fc
daydreamingwill <williamlatham13@yahoo.co.uk>
2015-01-26 10:35:22 +0000

    Update landmark.py

--- classes/landmark.py
+++ classes/landmark.py
@@ -1,23 +1,26 @@
-class Landmark:
-    
-    def __init__(self, Postion, Image, Size, Description, Searched):
-        self.Position = Postion
-        self.Image = Image
-        self.Size = Size
-        self.Description = Description
-        self.Searched = Searched
+# Initialize attributes for Landmark.
+    def __init__(self, name, size, pos, img, stat, desc):
+        Base.__init__(self, name, size, pos, img, stat): #Initialize inherited attributes, from Base.
+            self.decs = description
+            self.name = name
+            self.size = size
+            self.position = pos
+            self.image = img
+            self.state = stat
+            
+    def __searched: #Method to check if Landmark searched, then effectivly hides the tresure if True. Variable is private, as it is only to be used in the Landmark class.
 
-    def Display_is_landmark_searched(self):
-        print "test"
+        for Treasure in Treasure_list:
+            if self.position == Treasure:
+                return True
+            else:
+                return False         
+            
 
-    def Display_description(self):
-        print "test"
-    
+    def Treasure_present: #Method to check if treasure is present, by cross refrencing Landmark position with treasure position and seeing if Landmark already searched. 
 
-test = Landmark(1,1,1,"test",True)
-
-print test.Searched
-
-test.Display_is_landmark_searched()
-
-test.Display_description()
+        for Treasure in Treasure_list: #list of all locations of treasure objects
+            if self.position == Treasure and __searched == False: #Using a list of treasure location, the current Landmark can be cross refrenced with all treasure location to see if treasure is present.
+                return True
+            else:
+                return False
