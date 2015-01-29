cf1bcabc81c958ccaf8cf3756d71f9bd57f3dccc
Ak1001 <Ak1001@users.noreply.github.com>
2015-01-29 16:04:15 +0000

    Update treasure.py

--- classes/treasure.py
+++ classes/treasure.py
@@ -1,7 +1,4 @@
-import pygame
-from pygame.locals import *
-
-class Treasure():
+class Treasure(Base):
     def __init__(self,name = None, pos = None, size = None,imageHolder = None, pixMove = None, colour = None, movement = None, found= False,):  
         Base.__init__(self, name, pos, size, colour)
         self.imageHolder = imageHolder
@@ -9,15 +6,13 @@ class Treasure():
         self.found= found
         
         
-
+    """
     def __isFound(self):                                      
         if self.found== True:                     
             return treasure_found_serchead        
         else:
-            return not_searched
-
-    
-
+            return not_searched 
+    """
     def ShowingMessage(self):
       if dispayText == True:
           screenText = font.render("Treasure Found",True,(255,0,0))
