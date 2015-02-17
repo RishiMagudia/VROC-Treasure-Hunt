9c5027e676e3e7d0dddca462cee0821e3fa486a4
Eduard Kotkas <edkotkas@gmail.com>
2015-02-17 22:41:17 +0000

    Added object library manager library.py

---
+++ classes/library.py
@@ -0,0 +1,27 @@
+from landmark import Landmark
+from treasure import Treasure
+from trafficLights import trafficLights
+from robot import robot
+
+class Library:
+
+    def __init__(self):
+        """
+            Library for the objects that can be created.
+        """
+
+    def create(self, name, type, size, image):
+        """
+        Create object with id.
+        """
+
+
+    def select(self, id):
+        """
+        Select the object by id.
+        """
+
+    def remove(self, id):
+        """
+        Remove the object by id.
+        """
