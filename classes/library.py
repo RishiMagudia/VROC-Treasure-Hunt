324e3441a4c416b52a3a166af7516d6ba4387aca
edkotkas <edkotkas@gmail.com>
2015-03-06 01:19:06 +0000

    Updated library to SQL in library.py

--- classes/library.py
+++ classes/library.py
@@ -3,93 +3,68 @@ from treasure import Treasure
 from trafficLights import trafficLights
 from robot import robot
 
-#------------------------------------------------------------------
-#? replace with sql / +save & load features
-#! enhancement can be made with sql
-#------------------------------------------------------------------
+import sqlite3, sys
 
 class Library:
 
-    #! Global Constants -------------------------------------------
-    global KEY, NAME, TYPE, SIZE, IMAGE, POSITION
-
-    KEY = 0
-    NAME = 1
-    TYPE = 2
-    SIZE = 3
-    IMAGE = 4
-    POSITION = 5
-    # -------------------------------------------------------------
     def __init__(self):
         """
             Library for the objects that can be created.
         """
-        self.lib = []               #list of objects
+        self.connection = None
+        self.cursor = None
+        self.db = "data.sqlite"
+        try:
+            self.connection = sqlite3.connect(self.db)
+            self.cursor = self.connection.cursor()
 
-    # Library Handlers --------------------------------------------
+            self.cursor.execute('''CREATE TABLE data
+                                    (name TEXT,
+                                     type TEXT,
+                                     size INT,
+                                     image TEXT,
+                                     position TEXT)
+                                ''')
+        except sqlite3.Error, e:
+            print "Error %s:" % e.args[0]
+            # sys.exit(1)
 
-    def create(self, name, t, size, image, (x, y)):
-        """
-            Create object with key.
-        """
-        key = len(self.lib)+1       #unique identifier
-        self.lib.append([key, name, t, size, image, (x, y)])
+        finally:
+            if self.connection:
+                self.connection.close()
 
-    def selectByKey(self, key):
-        """
-            Select the object by key.
-        """
-        for i in self.lib:
-            if key == i[KEY]:
-                return i
+    def open(self):
+        try:
+            self.connection = sqlite3.connect(self.db)
+            self.cursor = self.connection.cursor()
+        except sqlite3.Error, e:
+            print "Error %s:" % e.args[0]
+            sys.exit(1)
 
-    def selectByType(self, t):
-        for i in self.lib:
-            if t == i[TYPE]:
-                return i
+    def close(self):
+        if self.connection:
+                self.connection.close()
 
-    def remove(self, key):
-        """
-            Remove the object by key.
-        """
-        self.lib.remove(self.selectByKey(key))
+    def insert(self, name, t, size, image, position):
+        self.open()
+        self.cursor.execute("INSERT INTO data VALUES (?, ?, ?, ?, ?)", (name, t, size, image, position))
+        self.connection.commit()
+        self.close()
 
-    def display(self):
-        """
-            Return the list of objects.
-        """
-        return self.lib
-    # -------------------------------------------------------------
+    def update_position(self, i, u):
+        self.open()
+        self.cursor.execute("UPDATE data SET position='%s' WHERE name='%s'" % (u, i))
+        self.connection.commit()
+        self.close()
 
-    # Getters/Setters for the object list. ------------------------
+    def remove(self, i, x):
+        self.open()
+        self.cursor.execute("DELETE * FROM data WHERE %s='%s'" % (i, x))
+        self.connection.commit()
+        self.close()
 
-    def getName(self, key):
-        return self.selectByKey(key)[NAME]
-
-    def setName(self, key, n):
-        self.selectByKey(key)[NAME] = n
-
-    def getType(self, key):
-        return self.selectByKey(key)[TYPE]
-
-    def setType(self, key, t):
-        self.selectByKey(key)[TYPE] = t
-
-    def getSize(self, key):
-        return self.selectByKey(key)[SIZE]
-
-    def setSize(self, key, s):
-        self.selectByKey(key)[SIZE] = s
-
-    def getImage(self, key):
-        return self.selectByKey(key)[IMAGE]
-
-    def setImage(self, key, i):
-        self.selectByKey(key)[IMAGE] = i
-
-    def getPosition(self, key):
-        return self.selectByKey(key)[POSITION]
-
-    def setPosition(self, key, (x, y)):
-        self.selectByKey(key)[POSITION] = (x, y)
-    # -------------------------------------------------------------
+    def select(self, i, x):
+        self.open()
+        r = self.cursor.execute("SELECT * FROM data WHERE %s='%s'" % (i ,x))
+        return r.fetchall()
+        self.close()
